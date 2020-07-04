2c705fd12d0465b11aae641c9a11143f7d2f17ba
shubham <shubhambhakuni26@gmail.com>
2020-07-04 23:06:03 +0530

    minor changes

--- lib/screens/boarding/SplashScreen.dart
+++ lib/screens/boarding/SplashScreen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shopconn/api/shopconnApi.dart';
+import 'package:shopconn/notifier/authNotifier.dart';
 import 'package:shopconn/screens/HomeScreen.dart';
 import 'package:shopconn/screens/boarding/boarding.dart';
 import 'package:shopconn/screens/login.dart';
@@ -14,6 +17,8 @@ class SplashScreen extends StatefulWidget {
 class SplashScreenState extends State<SplashScreen>
     with AfterLayoutMixin<SplashScreen> {
   Future checkFirstSeen() async {
+      AuthNotifier authNotifier =Provider.of<AuthNotifier>(context);
+    initializeCurrentUser(authNotifier);
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool _seen = (prefs.getBool('seen') ?? false);
     bool _logined = (prefs.getBool('logined') ?? false);
@@ -30,6 +35,8 @@ class SplashScreenState extends State<SplashScreen>
       Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => BoardingScreen()));
     }
+
+   
   }
 
   @override
